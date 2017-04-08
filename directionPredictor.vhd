4ea4026e980dafd9529a234dc06e3d5b8e45d5af
Yike Liu <okmilkd@gmail.com>
2017-04-07 20:26:04 -0400

    Add direction predictor

--- directionPredictor.vhd
+++ directionPredictor.vhd
@@ -6,6 +6,7 @@ USE ieee.numeric_std.all;
 --this is a 2-bit predictor
 entity directionPredictor is
 	Port(
+		clock: in std_logic;
 		op_code : in std_logic_vector(5 downto 0);
 		branch : in std_logic;
 		branch_actual : in std_logic;
@@ -15,69 +16,76 @@ entity directionPredictor is
 		--01 weak not taken
 		--10 weak taken
 		--11 strong taken
-		pred : out std_logic_vector(1 downto 0)
+		pred_s : out std_logic_vector(1 downto 0)
 		);
 end directionPredictor;
 
 architecture behaviour of directionPredictor is
-signal pred_t : std_logic_vector(1 downto 0);
+signal pred_t : std_logic_vector(1 downto 0):="00";
 signal op_code_t : std_logic_vector(5 downto 0);
 
 begin
-	pred <= pred_t;
+	pred_s <= pred_t;
 	op_code_t <= op_code;
 
-	process(pred_t)
+	process(clock)
 	begin
-		case pred_t is
-			when "00" =>
-				branch_pred <= '0';
+		if(falling_edge(clock) and branch = '1') then
+			case pred_t is
+				when "00" =>
+					branch_pred <= '0';
 
-			when "01" =>
-				branch_pred <= '0';
+				when "01" =>
+					branch_pred <= '0';
 
-			when "10" =>
-				branch_pred <= '1';
+				when "10" =>
+					branch_pred <= '1';
 
-			when "11" =>
-				branch_pred <= '1';
-		end case;
+				when "11" =>
+					branch_pred <= '1';
+			
+				when others =>
+					null;
+			end case;
+		end if;
 	end process;
 
-	process(branch)
+	process(clock)
 	begin
-		case pred_t is
-			when "00" =>
-				if (branch_actual = '0' and (op_code_t = "000100" or op_code_t = "000101") ) then
-					pred_t <= "00";
-				elsif (branch_actual = '1' and (op_code_t = "000100" or op_code_t = "000101")) then
-					pred_t <= "01";
-				end if;
+		if(rising_edge(clock) and branch ='1') then
+			case pred_t is
+				when "00" =>
+					if (branch_actual = '0' and (op_code_t = "000100" or op_code_t = "000101") ) then
+						pred_t <= "00";
+					elsif (branch_actual = '1' and (op_code_t = "000100" or op_code_t = "000101")) then
+						pred_t <= "01";
+					end if;
 
-			when "01" =>
-				if (branch_actual = '0' and (op_code_t = "000100" or op_code_t = "000101") ) then
-					pred_t <= "00";
-				elsif (branch_actual = '1' and (op_code_t = "000100" or op_code_t = "000101")) then
-					pred_t <= "10";
-				end if;
+				when "01" =>
+					if (branch_actual = '0' and (op_code_t = "000100" or op_code_t = "000101") ) then
+						pred_t <= "00";
+					elsif (branch_actual = '1' and (op_code_t = "000100" or op_code_t = "000101")) then
+						pred_t <= "10";
+					end if;
 
-			when "10" =>
-				if (branch_actual = '0' and (op_code_t = "000100" or op_code_t = "000101") ) then
-					pred_t <= "01";
-				elsif (branch_actual = '1' and (op_code_t = "000100" or op_code_t = "000101")) then
-					pred_t <= "11";
-				end if;
+				when "10" =>
+					if (branch_actual = '0' and (op_code_t = "000100" or op_code_t = "000101") ) then
+						pred_t <= "01";
+					elsif (branch_actual = '1' and (op_code_t = "000100" or op_code_t = "000101")) then
+						pred_t <= "11";
+					end if;
 
-			when "11" =>
-				if (branch_actual = '0' and (op_code_t = "000100" or op_code_t = "000101") ) then
-					pred_t <= "10";
-				elsif (branch_actual = '1' and (op_code_t = "000100" or op_code_t = "000101")) then
-					pred_t <= "11";
-				end if;
+				when "11" =>
+					if (branch_actual = '0' and (op_code_t = "000100" or op_code_t = "000101") ) then
+						pred_t <= "10";
+					elsif (branch_actual = '1' and (op_code_t = "000100" or op_code_t = "000101")) then
+						pred_t <= "11";
+					end if;
 
-			when others =>
-				null;
-		end case;
+				when others =>
+					null;
+			end case;
+		end if;
 	end process;
 
 end behaviour;
